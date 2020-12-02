0356dacd9bbac7fa9039f8c1df2dfce8a7ea338c
Maarten van Schaik <maartenvs@google.com>
2020-12-02 09:21:45 +0100

    Both parts of day 2 separate now.

--- src/day2/passwords.kt
+++ src/day2/passwords.kt
@@ -9,6 +9,13 @@ val lineFormat = """(\d+)-(\d+)\s(\w): (\w+)""".toRegex()
 fun main(args: Array<String>) {
   println(readLines(args[0]).filter { line ->
     val matchResult = lineFormat.find(line)
+    val (min, max, letter, password) = matchResult!!.destructured
+    val count = password.count { it == letter.toCharArray()[0] }
+    min.toInt() <= count && count <= max.toInt()
+  }.count())
+
+  println(readLines(args[0]).filter { line ->
+    val matchResult = lineFormat.find(line)
     val (first, second, letter, password) = matchResult!!.destructured
     (password[first.toInt() - 1] == letter.toCharArray()[0]) xor (password[second.toInt() - 1] == letter.toCharArray()[0])
   }.count())
