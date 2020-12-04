22a0c0b5b58d811232d7a4107e37aa420ae21f18
Maarten van Schaik <maartenvs@google.com>
2020-12-04 10:23:52 +0100

    Completed day 4

--- src/day4/passports.kt
+++ src/day4/passports.kt
@@ -3,13 +3,34 @@ package day4
 import java.io.File
 
 fun collectFields(line: String) =
-  line.split("\\s+".toRegex()).map { it.split(":".toRegex())[0] }.filter { it.isNotEmpty() }
+  line.split("\\s+".toRegex())
+    .filter { it.isNotEmpty() }
+    .map { it.split(":".toRegex()) }
+    .map { it[0] to it[1] }.toMap()
 
-val requiredFields = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" /*, "cid" */)
+val validators: Map<String, (String) -> Boolean> = mapOf(
+  "byr" to { s: String -> s.matches("\\d{4}".toRegex()) && s.toInt() in 1920 .. 2002 },
+  "iyr" to { s: String -> s.matches("\\d{4}".toRegex()) && s.toInt() in 2010 .. 2020 },
+  "eyr" to { s: String -> s.matches("\\d{4}".toRegex()) && s.toInt() in 2020 .. 2030 },
+  "hgt" to { s: String ->
+    val match = "(\\d+)(cm|in)".toRegex().matchEntire(s)
+    when (match?.groupValues?.get(2)) {
+      "cm" -> match.groupValues[1].toInt() in 150..193
+      "in" -> match.groupValues[1].toInt() in 59..76
+      else -> false
+    }
+  },
+  "hcl" to { s: String -> s.matches("#[0-9a-f]{6}".toRegex()) },
+  "ecl" to { s: String -> s.matches("(amb|blu|brn|gry|grn|hzl|oth)".toRegex()) },
+  "pid" to { s: String -> s.matches("\\d{9}".toRegex())}
+)
 
 fun main(args: Array<String>) {
-  val records = File(args[0]).readText().split("\n\n").map { collectFields(it) }
-  val validRecords = records.filter { it.toSet().containsAll(requiredFields) }
+  val records = File(args[0]).readText().split("\n\n").map(::collectFields)
+
+  val validRecords = records.filter {
+    validators.all { v -> it.containsKey(v.key) && v.value(it.getValue(v.key)) }
+  }
 
   println(validRecords.size)
 }
