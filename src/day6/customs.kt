d151ff5057807e1af6589a24661b856104dc2071
Maarten van Schaik <maartenvs@google.com>
2020-12-06 09:42:00 +0100

    Completed day 6.

--- src/day6/customs.kt
+++ src/day6/customs.kt
@@ -5,7 +5,9 @@ import java.io.File
 fun main(args: Array<String>) {
   println(
     File(args[0]).readText().split("\n\n")
-      .map{ it.toSet().filter(Char::isLetter) }
-      .map{ it.size }
+      .map {
+        it.lines()
+          .fold(('a'..'z').toSet()) { r, s -> r.intersect(s.asIterable()) } }
+      .map { it.size }
       .sum())
 }
