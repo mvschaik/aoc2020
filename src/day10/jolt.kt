dbe6c3cbaf7f80f42759529e7c65b0d8648a92d3
Maarten van Schaik <maartenvs@google.com>
2020-12-10 10:16:40 +0100

    Completed day 10.

--- src/day10/jolt.kt
+++ src/day10/jolt.kt
@@ -5,11 +5,20 @@ import java.io.File
 fun readLines(filename: String) = File(filename).readLines()
 
 fun main(args: Array<String>) {
-  val numbers = readLines(args[0]).map { it.toInt() }.sorted()
+  val input = readLines(args[0]).map { it.toInt() }.sorted()
+  val numbers = listOf(0) + input + listOf(input.last() + 3)
 
-  val diffs = (listOf(0) + numbers).zip(numbers)
+  val diffs = numbers.zip(numbers.drop(1))
     .fold(mutableMapOf<Int, Int>().withDefault { 0 }) {
         diffs, (a, b) -> diffs[b - a] = diffs.getValue(b - a) + 1; diffs }
-  diffs[3] = diffs.getValue(3) + 1
   println(diffs[3]!! * diffs[1]!!)
+
+  val waysToGetTo = MutableList(numbers.size) { 0L }
+  waysToGetTo[0] = 1
+  for ((i, n) in numbers.withIndex()) {
+    if (i > 0 && n - numbers[i-1] <= 3) waysToGetTo[i] += waysToGetTo[i-1]
+    if (i > 1 && n - numbers[i-2] <= 3) waysToGetTo[i] += waysToGetTo[i-2]
+    if (i > 2 && n - numbers[i-3] <= 3) waysToGetTo[i] += waysToGetTo[i-3]
+  }
+  println(waysToGetTo.last())
 }
