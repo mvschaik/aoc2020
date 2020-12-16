086a9d74492f3d500ae49820be2d084cd0764420
Maarten van Schaik <maartenvs@google.com>
2020-12-16 10:13:37 +0100

    Completed day 16.

--- src/day16/ticket.kt
+++ src/day16/ticket.kt
@@ -4,15 +4,14 @@ import java.io.File
 
 val fieldDesc = Regex("""(.*): (\d+)-(\d+) or (\d+)-(\d+)""")
 
-data class Field(val name: String, val ranges: List<IntRange>)
-typealias Ticket = List<Int>
+data class FieldDesc(val name: String, val ranges: List<IntRange>)
 
 fun main(args: Array<String>) {
   val (fieldsPar, yourPart, nearbyPart) = File(args[0]).readText().split("\n\n")
 
-  val fields = fieldsPar.lines().map { line ->
+  val fieldDescriptors = fieldsPar.lines().map { line ->
     val (name, s1, e1, s2, e2) = fieldDesc.matchEntire(line)!!.destructured
-    Field(name, listOf(s1.toInt()..e1.toInt(), s2.toInt()..e2.toInt()))
+    FieldDesc(name, listOf(s1.toInt()..e1.toInt(), s2.toInt()..e2.toInt()))
   }
 
   val your = yourPart.lines()[1].split(",").map { it.toInt() }
@@ -20,6 +19,37 @@ fun main(args: Array<String>) {
 
   println(nearby.flatMap {
       ticket -> ticket.filter {
-        f: Int -> fields.none {
-          fd: Field -> fd.ranges.any { it.contains(f) } } } }.sum())
+        f -> fieldDescriptors.none {
+          fd -> fd.ranges.any { it.contains(f) } } } }.sum())
+
+  val valid = nearby.filter {
+      ticket -> ticket.none {
+        f -> fieldDescriptors.none {
+          fd -> fd.ranges.any { it.contains(f) } } } }
+
+  val fields = arrayOfNulls<FieldDesc>(fieldDescriptors.size)
+  var fieldValues = valid.fold(List(your.size) { emptyList<Int>() }) {
+      acc, list -> acc.zip(list) { l, el -> l + el } }
+    .withIndex()
+  val descriptorsLeft = fieldDescriptors.toMutableSet()
+
+  while (fields.contains(null)) {
+    val matches = fieldValues.map { values ->
+      values.index to descriptorsLeft.filter {
+          fd -> values.value.all { value -> fd.ranges.any { range -> range.contains(value) } } }
+    }.filter { it.second.size == 1 }.map { it.first to it.second.single() }
+
+    if (matches.isEmpty()) break
+
+    for ((index, desc) in matches) {
+      fields[index] = desc
+      fieldValues = fieldValues.filter { it.index != index }
+      descriptorsLeft.remove(desc)
+    }
+  }
+
+  println(fields.withIndex()
+    .filter { it.value!!.name.startsWith("departure") }
+    .map { your[it.index].toLong() }
+    .reduce { a, b -> a * b })
 }
